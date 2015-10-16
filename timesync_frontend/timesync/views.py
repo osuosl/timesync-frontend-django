7d4b595b7eaa342c62a1bf39e3d3a4b415fddc38
MaraJade <catbookgirl@gmail.com>
2015-10-16 15:15:04 -0700

    Created view and template, currently just unlabled boxes

--- timesync_frontend/timesync/views.py
+++ timesync_frontend/timesync/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
+
+def submission(request):
+    template = loader.get_template('timesync/submission.html')
+    return HttpResponse(template.render())
+    #return render(request, 'timesync/submission.html')
