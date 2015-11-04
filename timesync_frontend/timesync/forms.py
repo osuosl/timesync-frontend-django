f931e1c656c8d4c095ded0ea9769139ffd0fcff9
MaraJade <catbookgirl@gmail.com>
2015-11-04 11:32:40 -0800

    Fixed forms.py so that it actually renders a form

--- timesync_frontend/timesync/forms.py
+++ timesync_frontend/timesync/forms.py
@@ -1,10 +1,15 @@
 from django import forms
 
 class TimeSubmissionForm(forms.Form): 
-    duration = forms.CharField(label='Duration'),
-    user = forms.CharField(label='User'),
-    project = forms.CharField(label='Project'),
-    activities = forms.CharField(label='Activities'),
-    notes = forms.CharField(label='Notes'),
-    issue_uri = forms.CharField(label='Issue URI'),
-    date_worked = forms.CharField(label='Date Worked'),
+    duration = forms.CharField(label='Duration')
+    user = forms.CharField(label='User')
+    project = forms.CharField(label='Project')
+    activities = forms.CharField(label='Activities')
+    notes = forms.CharField(label='Notes')
+    issue_uri = forms.CharField(label='Issue URI')
+    date_worked = forms.CharField(label='Date Worked')
+
+    class Meta:
+        fields = ['duration', 'user', 'project', 'activities', 'notes', 'issue_uri', 'date_worked']
+
+        exclude = ('podcast', 'size', 'length', 'part', 'mime_type')
--- timesync_frontend/timesync/views.py
+++ timesync_frontend/timesync/views.py
@@ -2,19 +2,18 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 
-from forms import TimeSubmissionForm
-# Create your views here.
+from timesync.forms import TimeSubmissionForm
 
 def time_submission(request):
     if request.method == 'POST':
         form = TimeSubmissionForm(request.POST)
         
         if form.is_valid():
+            #TODO
+            #Send to pymesync
             return HttpResponseRedirect('/timesync/submitted/')
     else:
         form = TimeSubmissionForm()
-
-    print form
 
     return render(request, 'timesync/time_submission_form.html', {'form': form})
 
