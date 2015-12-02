fa186e390645fadb270979771a99bf0c28fec463
MaraJade <catbookgirl@gmail.com>
2015-12-02 10:09:36 -0800

    Seems to be submitting and redirecting properly
    refs #7

--- timesync_frontend/timesync/forms.py
+++ timesync_frontend/timesync/forms.py
@@ -19,3 +19,13 @@ class TimeSubmissionForm(forms.Form):
 class LoginForm(forms.Form):
     username = forms.CharField(label='Username')
     password = forms.CharField(label='Password', widget=forms.PasswordInput())
+
+class TimeSelectionForm(forms.Form):
+    project = forms.CharField(label='Project', required=False)
+    user = forms.CharField(label='User', required=False)
+    activities = forms.CharField(label='Activities', required=False)
+    issue_uri = forms.URLField(label='Issue URI', required=False)
+    start = forms.CharField(label='Start', required=False)
+    end = forms.CharField(label='End', required=False)
+    revisions = forms.CharField(label='Revisions', required=False)
+    deleted = forms.CharField(label='Deleted', required=False)
